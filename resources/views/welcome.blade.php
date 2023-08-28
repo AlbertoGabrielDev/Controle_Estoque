16eba57ebd4152eea84691ba718c5d3818dd58b9
Alberto Gabriel <albertogabriel@norven.com.br>
2023-08-28 17:40:47 -0300

    primeiro projeto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.principal')
+
+@section('conteudo')
+    <button class="botão">Entrar/Cadastrar</button>
+    <button class="botão">Fornecedores</button>
+    <button class="botão">Estoque</button>
+    <button class="botão">Cadastrar usúario</button>
+
+    <div class="categoria">Categorias</div>
+@endsection
+
+
