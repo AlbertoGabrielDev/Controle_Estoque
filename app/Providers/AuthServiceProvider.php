7f8343b705e1449bd16dc3c3aeeb7cf5b996ef6a
Alberto Gabriel <albertogabriel@norven.com.br>
2023-09-21 18:18:25 -0300

    Autentificação de usuario feita

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Fortify\Http\Requests\LoginRequest;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+use Laravel\Fortify\Fortify;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -15,6 +19,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         Gate::define('permissao',function(User $user){
             return $user->id ===1;
+        });
+
+        Fortify::authenticateUsing(function (LoginRequest $request) {
+           
+            $user = User::where('email', $request->email)->first();
+           
+            if ($user &&
+                Hash::check($request->password, $user->password) && $user->status === 1 ) {
+               return $user;
+            }
         });
     }
 }
